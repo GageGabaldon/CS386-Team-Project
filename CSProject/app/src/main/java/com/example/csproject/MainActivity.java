2e428b684f08205cdb21285444d73a5eccfcd5d1
Gage Gabaldon <ggmangabaldon@gmail.com>
2020-02-26 16:44:25 -0700

    added a database with 7 chunguses

--- CSProject/app/src/main/java/com/example/csproject/MainActivity.java
+++ CSProject/app/src/main/java/com/example/csproject/MainActivity.java
@@ -2,9 +2,21 @@ package com.example.csproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.example.csproject.db.GameContract;
+import com.example.csproject.db.GameContractHelper;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +24,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createDatabase();
     }
+
+    private void createDatabase() {
+        // Create new helper
+        GameContractHelper dbHelper = new GameContractHelper(this);
+
+        // Get the database. If it does not exist, this is where it will
+        // also be created.
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+    }
+
+
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
         Intent intent = new Intent(this, MainPage2.class);
         startActivity(intent);
     }
+
+    /** sends to about activity */
     public void sendAbout(View view) {
         Intent intent = new Intent(this, About.class);
         startActivity(intent);
@@ -26,4 +54,7 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Settings.class);
         startActivity(intent);
     }
+
+
+
 }
