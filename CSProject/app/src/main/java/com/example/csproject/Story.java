7ccafb692e026fd7d6063909da34456c6e51e0f1
Gage Gabaldon <ggmangabaldon@gmail.com>
2020-03-05 12:16:50 -0700

    added a basic framework

---
+++ CSProject/app/src/main/java/com/example/csproject/Story.java
@@ -0,0 +1,22 @@
+package com.example.csproject;
+
+public class Story {
+    public Story(int event)
+    {
+
+    }
+
+    public String getChoices(int choiceNum)
+    {
+        return "";
+    }
+
+    public  String getCurrentText(){
+        return "";
+    }
+
+    public void newChoice(String choicemade)
+    {
+
+    }
+}
