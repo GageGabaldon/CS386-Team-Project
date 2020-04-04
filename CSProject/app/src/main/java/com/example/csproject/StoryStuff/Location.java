a60c29cf9f4735acdc4e9bc4a5e16f0fb1141989
elij48 <60413426+elij48@users.noreply.github.com>
2020-04-03 17:42:28 -0700

    class stubs

---
+++ CSProject/app/src/main/java/com/example/csproject/StoryStuff/Location.java
@@ -0,0 +1,8 @@
+package com.example.csproject.StoryStuff;
+
+public class Location
+{
+    String locationName;
+    int locationID;
+    String locDescription;
+}
---
+++ CSProject/app/src/main/java/com/example/csproject/StoryStuff/World.java
@@ -0,0 +1,13 @@
+package com.example.csproject.StoryStuff;
+
+public class World
+{
+    // fields
+    int playerLocation;
+    Event storyLocation;
+
+    public Location updatePlayerLocation(Story currentLocation)
+    {
+
+    }
+}
