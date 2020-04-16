bd2d48851c8808930c2b93322025da630bc729ca
Vidal Martinez <vrm55@nau.edu>
2020-04-16 00:40:01 -0700

    updates to the code as well as the data files for story

--- CSProject/app/src/main/java/com/example/csproject/StoryStuff/Event.java
+++ CSProject/app/src/main/java/com/example/csproject/StoryStuff/Event.java
@@ -1,10 +1,92 @@
 package com.example.csproject.StoryStuff;
 
-import java.util.List;
+import com.example.csproject.DataAccess;
+
 
 public class Event
 {
-    String playerDialogue;
-    String eventDialogue;
-    List NPCs;
+    /*
+    This class is in charge of providing the description of the event and the choices available for
+    this event. This object will be updated constantly, member states will change.
+     */
+    private int eventID;
+    private String eventDescription = "";
+    private String[] choices = new String[ 4 ];
+
+    // buffer stores data for event or choices temporarily
+    private DataAccess accessBuffer = new DataAccess();
+
+    public void setEventID( int newID )
+    {
+        eventID = newID;
+    }
+
+    public int getEventID()
+    {
+        return eventID;
+    }
+
+    public String getDescription()
+    {
+        return eventDescription;
+    }
+
+    public String[] getChoices()
+    {
+        return choices;
+    }
+
+    public void updateDescription( int currentStory, int currentEventID )
+    {
+        // eventBuffer will get string from file, and return the value to the eventDescription
+        eventDescription = accessBuffer.getData( currentStory, currentEventID );
+    }
+
+    public void updateChoices( int currentStory, int currentEventID )
+    {
+        // choiceBuffer will return a string array which will need to be passed out to each choice
+        String[] tempChoices = accessBuffer.getData( currentStory, currentEventID );
+        int cIndex = 0;
+        for( String choice : tempChoices )
+        {
+            choices[ cIndex ] = choice;
+            cIndex++;
+        }
+    }
+
+    /**
+    * Method receives the choice made, and the current event in the story.
+    * Updates description and choices available for the player.
+    * Returns the ID for the next event.
+     */
+    public void updateCurrentEvent( int currentStory, int choiceMade, int currentEventID )
+    {
+     // verify choices
+        // function: verifyChoices -> Temporarily: provide next event in story
+        eventID = currentEventID++;
+        // uses updateChoices and updateDescription
+        updateDescription( currentStory, eventID );
+        updateChoices( currentStory, eventID );
+    }
+
+    /**
+    * Method uses world factors to verify the next event(s) the player can proceed to
+    * **Unfinished, bypass for now**
+     */
+    public int verifyChoice( int[] choicesMade )
+    {
+        // computes next event possible based on all previously
+        int playerMoralStanding = 0;
+        for( int choice : choicesMade )
+        {
+            playerMoralStanding += choice;
+        }
+        /*
+        NOTICE:
+        Still in the process of finding a cool and efficient way to decide what the player can do next
+        given their choices made thus far, the current event, and perhaps other factors.
+         */
+        // return next event
+        return -1;
+    }
 }
--- CSProject/app/src/main/java/com/example/csproject/StoryStuff/World.java
+++ CSProject/app/src/main/java/com/example/csproject/StoryStuff/World.java
@@ -1,13 +1,90 @@
 package com.example.csproject.StoryStuff;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+    /**
+     * This class provides the latest information on the story state. The events, past and current.
+     * The current description and the available choices as well as choices made.
+     */
 public class World
 {
-    // fields
-    int playerLocation;
-    Event storyLocation;
+    // Plan to implement class to read directories and return file names in strings to remove hard coding
+    //Also, this should probably be placed in DataAccess so that class will have a reference for the hashtable
+    private Dictionary<Double, String> fileNameKeys = new Hashtable<Double, String>(){
+                                                        { put(1.0, "Introduction.txt");
+                                                          put(2.0, "CoffeeShopIntro.txt"); // dependent on the choice in 2.0, you go to either 2.1,.2,.3, or .4 (notice this, button 1, 2, 3, 4) until you go to 3.0 and story ends.
+                                                          put(2.1, "CoffeeShopPath1.txt");
+                                                          put(2.2, "CoffeeShopPath2.txt");
+                                                          put(2.3, "CoffeeShopPath3.txt");
+                                                          put(2.4, "CoffeeShopPath4.txt");
+                                                          put(3.0, "CoffeeShopOutro.txt");
+                                                        }
+    };
+    /* Idea: Maybe each storyline should be kept in a separate file with a master story directory
+    This could be useful for event ID numbering, we wont have large or sub level numbers to worry about.
+    Ex. If we don't separate the stories, we will have story 1 with events n, 1.n and if we use choices
+    in this as m, this could be a 1.n.m. Instead, we keep track of what story folder we are in and
+    can just focus on a single number 1 for event one and so on.
+     */
+    private ArrayList<Integer> playedStories;
+    private ArrayList<Location> allLocations;
+    private ArrayList<Integer> choicesMade;
+    //private Player player;
+    private Story currentStory;
 
-    public Location updatePlayerLocation(Story currentLocation)
+    public World()
     {
-        return null;
+        // Story object is created which instantiates the first event in the story
+        currentStory = new Story( 0 );
+        // construct world locations
+
+    }
+    public void updateLocation()
+    {
+        // knowing the current story
+
+    }
+
+    public void constructLocationList()
+    {
+        // another hashtable with <name: string, description: string> ? or perhaps use IDs?
+    }
+
+    /**
+     * adds choice made to choices made array list
+      */
+    public void updateChoicesMade( ArrayList<Integer> choicesMade, int choiceMade )
+    {
+        choicesMade.add( choiceMade );
+    }
+
+    /**
+    * adds current story to played stories array if not already in it, then updates the events for the current story.
+    * If there are no events left, the story needs to end and return to the "hub" or precinct.
+     */
+    public void updateStory( ArrayList<Integer> choicesMade, int currentEventID )
+    {
+        if( !playedStories.contains( currentStory ) )
+            playedStories.add( currentStory.getCurrentStoryID() );
+        if( hasEvents())
+        currentStory.updateEvents( choicesMade.get( choicesMade.size() ) );
+
+    }
+
+    /**
+    * For now, this check only accounts for a linear progression through the story ( 1 -> 2 -> 3 -> etc )
+    * The plan will be to have this more complex to make story progression more interesting.
+     */
+    public boolean hasEvents()
+    {
+        return currentStory.getEventID() < fileNameKeys.size();
+    }
+
+    public void updatePlayer()
+    {
+
     }
 }
